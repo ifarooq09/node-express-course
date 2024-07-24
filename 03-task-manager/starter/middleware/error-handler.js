6b72c80a2262ab071539c359e67243c211bc566e
Ibrahim <ifarooq9@gmail.com>
2024-07-24 08:54:01 +0430

    The key change I broguht was adding next to the controller functions so that errors can be properly forwarded to the error handling middleware

--- 03-task-manager/starter/middleware/error-handler.js
+++ 03-task-manager/starter/middleware/error-handler.js
@@ -1,10 +1,10 @@
-const {CustomAPIError} = require('../errors/custom-error.js')
+const { CustomAPIError } = require('../errors/custom-error.js');
 
-const errorHandlerMiddlware = (err, req, res, next) => {
+const errorHandlerMiddleware = (err, req, res, next) => {
     if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({ msg: err.message})
+        return res.status(err.statusCode).json({ msg: err.message });
     }
-    return res.status(500).json({ msg: "Something went wrong, try agian later" })
-}
+    return res.status(500).json({ msg: "Something went wrong, try again later" });
+};
 
-module.exports = errorHandlerMiddlware
+module.exports = errorHandlerMiddleware;
